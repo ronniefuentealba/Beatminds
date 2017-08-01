38e26ef7e41610fd6604e41a2992837e6db83cfb
Leonardo Vega <leonardo.vega@globant.com>
2017-07-31 22:20:10 -0400

    menu removed

--- src/js/beatminds.js
+++ src/js/beatminds.js
@@ -7,4 +7,10 @@ $(function () {
     var $nav = $(".navbar");
     $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
   });
+  if($('.nav-item.dropdown').hasClass('show')){
+    $('.nav-link.dropdown-toggle').addClass('fa-rotate-180');
+  }
+  else{
+    $('.nav-link.dropdown-toggle').removeClass('fa-rotate-180');
+  }
 });
